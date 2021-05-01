ab9488c8b4f851bb57a3af5112130d698214f4dd
Md Jahidul Islam <iamjahid697@gmail.com>
2021-05-02 01:28:33 +0600

    added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,12 +3,38 @@ import React from 'react';
 const Cart = (props) => {
     const cart=props.cart;
     console.log(cart)
-    const total = cart.reduce( (total ,prd) =>total + prd.price ,0)
+    // const total = cart.reduce( (total ,prd) =>total + prd.price ,0)
+    let total = 0;
+    for(let i =0;i<cart.length; i++){
+        const product =cart[i];
+        total=total + product.price;
+    }
+
+    let shipping =0;
+    if(total > 35){
+        shipping =0
+    }
+    else if(total> 15){
+        shipping = 4.99
+    }
+    else if(total>0){
+        shipping = 12.99
+    }
+    const tax=(total/ 10).toFixed(2);
+    const grandTotal =(total + shipping + Number(tax)).toFixed(2);
+
+    const formatNumber =num =>{
+        const precision =num.toFixed(2);
+        return Number(precision);
+    }
     return (
         <div>
             <h3>Order summary</h3>
             <p> Order iteams :{cart.length}</p>
-            <p>Total price :{total}</p>
+            <p>Product Price:{formatNumber(total)}</p>
+            <p><small>Shipping Cost:{shipping}</small></p>
+            <p><small>Tax +Vat:{tax}</small></p>
+            <p>Total price :{grandTotal}</p>
         </div>
     );
 };
